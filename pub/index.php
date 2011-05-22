fad763d2f3de5310ee56ce8c64936ddeb6a131cd
Tim Weber <t.weber@digi-info.de>
2011-05-22 12:35:08 +0200

    Add build_params()

--- pub/index.php
+++ pub/index.php
@@ -23,12 +23,34 @@ function request_path() {
 	return trim(preg_replace('/\?.*$/', '', $_SERVER['REQUEST_URI']), '/');
 }
 
+function build_params($def) {
+	$args = isset($def['args']) ? $def['args'] : array();
+	$result = array();
+	foreach ($args as $arg => $cfg) {
+		if (!isset($_GET[$arg])) {
+			if (!isset($cfg['optional']) || !$cfg['optional']) {
+				throw new Exception("Missing required parameter: $arg");
+			}
+		} else {
+			// TODO: validation
+			$result[] = $_GET[$arg];
+		}
+	}
+	return $result;
+}
+
 function do_action() {
 	$definition = route(request_path());
 	if ($definition !== false) {
 		// TODO: Check if callback exists at all before calling.
 		// TODO: Pass actual parameters, if any.
-		$result = call_user_func_array($definition['callback'], array());
+		$params = array();
+		try {
+			$params = build_params($definition);
+		} catch (Exception $e) {
+			return array('error' => array('code' => 400, 'message' => $e->getMessage()));
+		}
+		$result = call_user_func_array($definition['callback'], $params);
 		if (!is_array($result)) {
 			return array('error' => array('code' => 500, 'message' => 'No array returned'));
 		}
